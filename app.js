459fe40c6b452037398c624b6c4cdfa7c2cee540
DanielDuesenDieb <110566119+DanielDuesenDieb@users.noreply.github.com>
2023-09-24 17:19:32 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,111 @@
+$(document).ready(function(){
+    var logs = 0;
+    var stone = 0;
+    var pickaxes = 0;
+    var money = 0;
+    var logPlus = 1;
+    var logPrice = 1;
+    var menu;
+    var autochopper = false;
+
+
+    $("#chop").click(function(){
+        logs += logPlus;
+        changeInventory();
+        changeMarket();
+    });
+
+    $("#sell1").click(function(){
+        logs--;
+        money += logPrice;
+        changeInventory();
+        changeMarket();
+    });
+
+    $("#sell10").click(function(){
+        logs-=10;
+        money += logPrice * 10;
+        changeInventory();
+        changeMarket();
+    });
+
+    $("#sellAll").click(function(){
+        money += logPrice * logs;
+        logs = 0;
+        changeInventory();
+        changeMarket();
+    });
+
+
+    $("#visit").click(function(){
+        menu = switchMenu("marketplace");
+        changeMarket();
+    });
+
+    $("#return").click(function(){
+        menu = switchMenu("main");
+    });
+
+
+    function changeInventory(){
+        $("#money").html("Money: $" + money);
+
+        if(logs == 1){
+            $("#logs").html("You now own " + logs + " log.");
+        }else{
+            $("#logs").html("You now own " + logs + " logs.");
+        }
+
+        if(stone > 0){
+            $("#stone").html("You now own " + stone + " piece(s) of stone.");
+        }else{
+            $("#stone").html("");
+        }
+
+        if(pickaxes > 0){
+            $("#pickaxes").html("You now own " + pickaxes + " pickaxe(s).");
+        }else{
+            $("#pickaxes").html("");
+        }
+    }
+
+    function changeMarket(){
+        if(logs > 0){
+            $("#sellAll").css("display", "block");
+        }else{
+            $("#sellAll").css("display", "none");
+        }
+        if(logs >= 1){
+            $("#sell1").css("display", "block");
+        }else{
+            $("#sell1").css("display", "none");
+        }
+        if(logs >= 10){
+            $("#sell10").css("display", "block");
+        }else{
+            $("#sell10").css("display", "none");
+        }
+    }
+
+    function switchMenu(menu){
+        $(".menus").children().css("display", "none");
+        $("." + menu).css("display", "block");
+        return menu;
+    }
+
+    $("#autoChoppertoggel").click(function() {
+        autochopper = !autochopper; // Toggle the autochopper flag
+        if (autochopper) {
+            autoChopperLoop(); // Start the loop
+        }
+    });
+    
+    function autoChopperLoop() {
+        if (autochopper) {
+            logs += logPlus;
+            changeInventory();
+            changeMarket();
+            setTimeout(autoChopperLoop, 1000); // Repeat the loop after 1 second (1000 milliseconds)
+        }
+    }
+});
